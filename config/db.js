2ac2c0df67bc521f37866f365db91bd6d02f0eb8
Gloriah-B <Gloriah-B@github.com>
2024-10-13 13:34:53 -0700

    updates

--- config/db.js
+++ config/db.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB Connected...');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);  // Exit process with failure
-  }
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('MongoDB Connected...');
+    } catch (error) {
+        console.error('MongoDB connection failed:', error);
+        process.exit(1); // Exit process with failure
+    }
 };
 
 module.exports = connectDB;
